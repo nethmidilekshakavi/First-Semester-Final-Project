0ee0e9923c2487e5138968ee0d1bd5e48b047b89
nethmidilekshakavi <nethmidileksha6@gmail.com>
2024-04-13 18:32:33 +0530

    newLoginPage

--- src/main/java/lk/ijse/controller/LoginForm.java
+++ src/main/java/lk/ijse/controller/LoginForm.java
@@ -2,10 +2,6 @@ package lk.ijse.controller;
 
 import java.io.IOException;
 import java.net.URL;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -19,7 +15,6 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
-import lk.ijse.DB.DbConnetion;
 
 public class LoginForm {
 
@@ -45,16 +40,19 @@ public class LoginForm {
     private Text ResaurentName;
 
     @FXML
-    private ImageView arrow;
-
-    @FXML
     private ImageView eye;
 
     @FXML
     private Button login;
 
     @FXML
-    private ImageView loginPage;
+    private ImageView loginPageGreen;
+
+    @FXML
+    private ImageView loginPagePic;
+
+    @FXML
+    private AnchorPane pane;
 
     @FXML
     private Text password;
@@ -74,8 +72,8 @@ public class LoginForm {
         String password = "1234";
 
         if (txtun.getText().equals(username) && txtpw.getText().equals(password)) {
-            Parent parent = FXMLLoader.load(getClass().getResource("/view/dashboard.fxml"));
-            Scene scene = new Scene(parent);
+            Object parent = FXMLLoader.load(getClass().getResource("/view/dashboard.fxml"));
+            Scene scene = new Scene((Parent) parent);
             Stage stage = new Stage();
             stage.setScene(scene);
             stage.setTitle("DashBoard");
@@ -89,6 +87,7 @@ public class LoginForm {
         else if(txtpw.getText()!=(password)) {
             new Alert(Alert.AlertType.INFORMATION, "sorry! user password can't be find!").show();
         }
+
     }
 
     @FXML
@@ -98,16 +97,16 @@ public class LoginForm {
         assert GOOGLE != null : "fx:id=\"GOOGLE\" was not injected: check your FXML file 'Login.fxml'.";
         assert OR != null : "fx:id=\"OR\" was not injected: check your FXML file 'Login.fxml'.";
         assert ResaurentName != null : "fx:id=\"ResaurentName\" was not injected: check your FXML file 'Login.fxml'.";
-        assert arrow != null : "fx:id=\"arrow\" was not injected: check your FXML file 'Login.fxml'.";
         assert eye != null : "fx:id=\"eye\" was not injected: check your FXML file 'Login.fxml'.";
         assert login != null : "fx:id=\"login\" was not injected: check your FXML file 'Login.fxml'.";
-        assert loginPage != null : "fx:id=\"loginPage\" was not injected: check your FXML file 'Login.fxml'.";
+        assert loginPageGreen != null : "fx:id=\"loginPageGreen\" was not injected: check your FXML file 'Login.fxml'.";
+        assert loginPagePic != null : "fx:id=\"loginPagePic\" was not injected: check your FXML file 'Login.fxml'.";
+        assert pane != null : "fx:id=\"pane\" was not injected: check your FXML file 'Login.fxml'.";
         assert password != null : "fx:id=\"password\" was not injected: check your FXML file 'Login.fxml'.";
         assert txtpw != null : "fx:id=\"txtpw\" was not injected: check your FXML file 'Login.fxml'.";
         assert txtun != null : "fx:id=\"txtun\" was not injected: check your FXML file 'Login.fxml'.";
         assert username != null : "fx:id=\"username\" was not injected: check your FXML file 'Login.fxml'.";
+
     }
+
 }
-
-
-
